dc4c31caf121df415a34ce8bc084027de407bc80
Eric McNiece <emcniece@caorda.com>
2014-11-28 10:55:35 -0800

    Adding shortcode, moving content generation to separate function

--- visual-sitemap.php
+++ visual-sitemap.php
@@ -35,6 +35,7 @@ class Visual_Sitemap {
 		add_action( 'init', array( $this, 'init' ) );
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'admin_styles' ) );
 	}
 
 	/**
@@ -57,6 +58,9 @@ class Visual_Sitemap {
 	public function init() {
 		// Set up localisation
 		$this->load_plugin_textdomain();
+
+		// Set up shortcode
+		add_shortcode( 'visual-sitemap', array(&$this, 'sitemap_content') );
 	}
 
 	/**
@@ -80,6 +84,151 @@ class Visual_Sitemap {
 		add_menu_page( __( 'Sitemap', 'visual-sitemap' ), __( 'Sitemap', 'visual-sitemap' ), 'manage_categories', 'visual-sitemap', array( $this, 'page_content' ), 'dashicons-networking' );
 	}
 
+	/**
+	 * Shortcode handler
+	 */
+	public function sitemap_content(){
+		$output .= '<div class="visual-sitemap">';
+
+		if ( ! isset( $_GET['taxonomy'] ) ): // Pages
+
+			$pages = get_pages( array( 'sort_column' => 'menu_order' ) );
+			$child_pages = array();
+
+			// Loop through pages to find child pages
+			foreach ( $pages as $index => $page ):
+				if ( $page->post_parent ):
+					if ( ! array_key_exists( $page->post_parent, $child_pages ) ) $child_pages[ $page->post_parent ] = array();
+					$child_pages[ $page->post_parent ][] = $page;
+					unset( $pages[ $index ] );
+				endif;
+			endforeach;
+
+			$object = get_post_type_object( 'page' );
+			$category = get_taxonomy( 'category' );
+			$tag = get_taxonomy( 'post_tag' );
+
+
+			$output .= '<ul class="vs-utility">
+				<li><a class="button" href="'. esc_url( admin_url( add_query_arg( array( 'page' => 'visual-sitemap', 'taxonomy' => 'category' ), 'admin.php' ) ) ) .'">'. $category->labels->name.'</a></li>
+				<li><a class="button" href="'. esc_url( admin_url( add_query_arg( array( 'page' => 'visual-sitemap', 'taxonomy' => 'post_tag' ), 'admin.php' ) ) ) .'">'. $tag->labels->name .'</a></li>
+			</ul>
+
+			<ul class="vs-primary col'. (max( sizeof( $pages ), 1 ) + 1).'">
+				<li class="vs-home"><a class="button disabled">'. get_bloginfo( 'name' ).'<span class="dashicons dashicons-admin-home"></span></a></li>';
+			if ( sizeof( $pages ) ):
+				foreach ( $pages as $page ):
+					$output .= '<li><a class="button button-primary action" href="'. esc_url( get_edit_post_link( $page->ID ) ).'">'. wp_strip_all_tags( $page->post_title ).'<span class="dashicons dashicons-admin-page"></span></a>';
+					if ( array_key_exists( $page->ID, $child_pages ) ): $children = $child_pages[ $page->ID ];
+						$output .= '<ul>';
+						foreach ( $children as $child ):
+							$output .= '<li><a class="button action" href="'. esc_url( get_edit_post_link( $child->ID ) ).'">'. wp_strip_all_tags( $child->post_title ).'<span class="dashicons dashicons-admin-page wp-ui-text-notification"></span></a>';
+							if ( array_key_exists( $child->ID, $child_pages ) ): $grandchildren = $child_pages[ $child->ID ];
+								$output .= '<ul>';
+								foreach ( $grandchildren as $grandchild ):
+									$output .= '<li><a class="button action" href="'. esc_url( get_edit_post_link( $grandchild->ID ) ).'">'. wp_strip_all_tags( $grandchild->post_title ).'<span class="dashicons dashicons-admin-page wp-ui-text-highlight"></span></a></li>';
+								endforeach;
+								$output .= '</ul>';
+							endif;
+							$output .= '</li>';
+						endforeach;
+						$output .= '</ul>';
+					endif;
+					$output .= '</li>';
+				endforeach;
+			else:
+				$output .= '<li><a class="button disabled">'. $object->labels->not_found.'</a></li>';
+			endif;
+
+			if( is_admin()):
+				$output .= '<li><a class="button button-primary action" href="'. esc_url( admin_url( add_query_arg( array( 'post_type' => 'page' ), 'post-new.php' ) ) ).'">'. $object->labels->add_new_item.'<span class="dashicons dashicons-plus"></span></a></li>';
+			endif;
+
+			$output .= '</ul>';
+
+		else: // Posts
+
+			$page = get_post_type_object( 'page' );
+			$taxonomy = $_GET['taxonomy'];
+			if ( $taxonomy == 'category' ):
+				$category = $object = get_taxonomy( 'category' );
+				$tag = get_taxonomy( 'post_tag' );
+				$terms = get_categories();
+				$sub_terms = array();
+
+				// Loop through categories to find subcategories
+				foreach ( $terms as $index => $term ):
+					if ( $term->parent ):
+						if ( ! array_key_exists( $term->parent, $sub_terms ) ) $sub_terms[ $term->parent ] = array();
+						$sub_terms[ $term->parent ][] = $term;
+						unset( $terms[ $index ] );
+					endif;
+				endforeach;
+			else:
+				$terms = get_tags( array( 'parent' => 0 ) );
+				$category = get_taxonomy( 'category' );
+				$tag = $object = get_taxonomy( 'post_tag' );
+			endif;
+
+
+			$output .= '<ul class="vs-utility">';
+			$output .= '<li><a class="button" href="'. esc_url( admin_url( add_query_arg( array( 'page' => 'visual-sitemap' ), 'admin.php' ) ) ).'">'. $page->labels->name.'</a></li>';
+			if ( $taxonomy == 'category' ):
+				$output .= '<li><a class="button" href="'. esc_url( admin_url( add_query_arg( array( 'page' => 'visual-sitemap', 'taxonomy' => 'post_tag' ), 'admin.php' ) ) ).'">'. $tag->labels->name.'</a></li>';
+			else:
+				$output .= '<li><a class="button" href="'.  esc_url( admin_url( add_query_arg( array( 'page' => 'visual-sitemap', 'taxonomy' => 'category' ), 'admin.php' ) ) ).'">'. $category->labels->name.'</a></li>';
+			endif;
+			$output .= '</ul>';
+
+			$output .= '<ul class="vs-primary col'.  (max( sizeof( $terms ), 1 ) + 1).'">';
+			$output .= '<li class="vs-home"><a class="button disabled">'. get_bloginfo( 'name' ).'<span class="dashicons dashicons-admin-home"></span></a></li>';
+			if ( sizeof( $terms ) ):
+				foreach ( $terms as $term ):
+					$output .= '<li><a class="button button-primary action" href="'. esc_url( admin_url( add_query_arg( array( 'action' => 'edit', 'taxonomy' => $taxonomy, 'tag_ID' => $term->term_id, 'post_type' => 'post' ), 'edit-tags.php' ) ) ).'">'. wp_strip_all_tags( $term->name ).'<span class="dashicons dashicons-'. $taxonomy == 'category' ? 'category' : 'tag'.'"></span></a>';
+
+					if ( $taxonomy == 'category' )
+						$posts = get_posts( array( 'category' => $term->term_id ) );
+					else
+						$posts = get_posts( array( 'tag' => $term->slug ) );
+
+					if ( $posts ):
+						$output .= '<ul>';
+							foreach ( $posts as $post ):
+								$output .= '<li><a class="button action" href="'. esc_url( get_edit_post_link( $post->ID ) ).'">'. wp_strip_all_tags( $post->post_title ).'<span class="dashicons dashicons-admin-page wp-ui-text-notification"></span></a></li>';
+							endforeach;
+						$output .= '</ul>';
+					endif;
+					if ( $taxonomy == 'category' && array_key_exists( $term->term_id, $sub_terms ) ): $subcategories = $sub_terms[ $term->term_id ];
+						if ( $subcategories ):
+							$output .= '<ul>';
+							foreach ( $subcategories as $subcategory ):
+								$output .= '<li><a class="button action" href="'. esc_url( admin_url( add_query_arg( array( 'action' => 'edit', 'taxonomy' => $taxonomy, 'tag_ID' => $subcategory->term_id, 'post_type' => 'post' ), 'edit-tags.php' ) ) ).'">'. wp_strip_all_tags( $subcategory->name ).'<span class="dashicons dashicons-'. $taxonomy == 'category' ? 'category' : 'tag'.' wp-ui-text-highlight"></span></a>';
+								$posts = get_posts( array( $taxonomy => $subcategory->term_id ) );
+								if ( $posts ):
+									$output .= '<ul>';
+									foreach ( $posts as $post ):
+										$output .= '<li><a class="button action" href="'. esc_url( get_edit_post_link( $post->ID ) ).'">'. wp_strip_all_tags( $post->post_title ).'<span class="dashicons dashicons-admin-page wp-ui-text-notification"></span></a></li>';
+									endforeach;
+									$output .= '</ul>';
+								endif;
+								$output .= '</li>';
+							endforeach;
+							$output .= '</ul>';
+						endif;
+					endif;
+					$output .= '</li>';
+				endforeach;
+			else:
+				$output .= '<li><a class="button disabled">'. $object->labels->not_found.'</a></li>';
+			endif;
+			$output .= '<li><a class="button button-primary action" href="'. esc_url( admin_url( add_query_arg( array( 'taxonomy' => $taxonomy ), 'edit-tags.php' ) ) ).'">'. $object->labels->add_new_item.'<span class="dashicons dashicons-plus"></span></a></li>';
+			$output .= '</ul>';
+
+		endif;
+		$output .= '</div>';
+
+		return $output;
+	}
 
 	/**
 	 * Page content
@@ -90,142 +239,9 @@ class Visual_Sitemap {
 			<h2>
 				<?php _e( 'Sitemap', 'visual-sitemap' ); ?>
 			</h2>
-			<div class="visual-sitemap">
 
-				<?php if ( ! isset( $_GET['taxonomy'] ) ): // Pages ?>
+			<?php echo $this->sitemap_content(); ?>
 
-					<?php
-					$pages = get_pages( array( 'sort_column' => 'menu_order' ) );
-					$child_pages = array();
-
-					// Loop through pages to find child pages
-					foreach ( $pages as $index => $page ):
-						if ( $page->post_parent ):
-							if ( ! array_key_exists( $page->post_parent, $child_pages ) ) $child_pages[ $page->post_parent ] = array();
-							$child_pages[ $page->post_parent ][] = $page;
-							unset( $pages[ $index ] );
-						endif;
-					endforeach;
-
-					$object = get_post_type_object( 'page' );
-					$category = get_taxonomy( 'category' );
-					$tag = get_taxonomy( 'post_tag' );
-					?>
-
-					<ul class="vs-utility">
-						<li><a class="button" href="<?php echo esc_url( admin_url( add_query_arg( array( 'page' => 'visual-sitemap', 'taxonomy' => 'category' ), 'admin.php' ) ) ); ?>"><?php echo $category->labels->name; ?></a></li>
-						<li><a class="button" href="<?php echo esc_url( admin_url( add_query_arg( array( 'page' => 'visual-sitemap', 'taxonomy' => 'post_tag' ), 'admin.php' ) ) ); ?>"><?php echo $tag->labels->name; ?></a></li>
-					</ul>
-
-					<ul class="vs-primary col<?php echo max( sizeof( $pages ), 1 ) + 1; ?>">
-						<li class="vs-home"><a class="button disabled"><?php bloginfo( 'name' ); ?><span class="dashicons dashicons-admin-home"></span></a></li>
-						<?php if ( sizeof( $pages ) ): ?>
-							<?php foreach ( $pages as $page ): ?>
-								<li><a class="button button-primary action" href="<?php echo esc_url( get_edit_post_link( $page->ID ) ); ?>"><?php echo wp_strip_all_tags( $page->post_title ); ?><span class="dashicons dashicons-admin-page"></span></a>
-									<?php if ( array_key_exists( $page->ID, $child_pages ) ): $children = $child_pages[ $page->ID ]; ?>
-										<ul>
-											<?php foreach ( $children as $child ): ?>
-												<li><a class="button action" href="<?php echo esc_url( get_edit_post_link( $child->ID ) ); ?>"><?php echo wp_strip_all_tags( $child->post_title ); ?><span class="dashicons dashicons-admin-page wp-ui-text-notification"></span></a>
-													<?php if ( array_key_exists( $child->ID, $child_pages ) ): $grandchildren = $child_pages[ $child->ID ]; ?>
-														<ul>
-															<?php foreach ( $grandchildren as $grandchild ): ?>
-																<li><a class="button action" href="<?php echo esc_url( get_edit_post_link( $grandchild->ID ) ); ?>"><?php echo wp_strip_all_tags( $grandchild->post_title ); ?><span class="dashicons dashicons-admin-page wp-ui-text-highlight"></span></a></li>
-															<?php endforeach; ?>
-														</ul>
-													<?php endif; ?>
-												</li>
-											<?php endforeach; ?>
-										</ul>
-									<?php endif; ?>
-								</li>
-							<?php endforeach; ?>
-						<?php else: ?>
-							<li><a class="button disabled"><?php echo $object->labels->not_found; ?></a></li>
-						<?php endif; ?>
-						<li><a class="button button-primary action" href="<?php echo esc_url( admin_url( add_query_arg( array( 'post_type' => 'page' ), 'post-new.php' ) ) ); ?>"><?php echo $object->labels->add_new_item; ?><span class="dashicons dashicons-plus"></span></a></li>
-					</ul>
-
-				<?php else: // Posts ?>
-
-					<?php
-					$page = get_post_type_object( 'page' );
-					$taxonomy = $_GET['taxonomy'];
-					if ( $taxonomy == 'category' ):
-						$category = $object = get_taxonomy( 'category' );
-						$tag = get_taxonomy( 'post_tag' );
-						$terms = get_categories();
-						$sub_terms = array();
-
-						// Loop through categories to find subcategories
-						foreach ( $terms as $index => $term ):
-							if ( $term->parent ):
-								if ( ! array_key_exists( $term->parent, $sub_terms ) ) $sub_terms[ $term->parent ] = array();
-								$sub_terms[ $term->parent ][] = $term;
-								unset( $terms[ $index ] );
-							endif;
-						endforeach;
-					else:
-						$terms = get_tags( array( 'parent' => 0 ) );
-						$category = get_taxonomy( 'category' );
-						$tag = $object = get_taxonomy( 'post_tag' );
-					endif;
-					?>
-
-					<ul class="vs-utility">
-						<li><a class="button" href="<?php echo esc_url( admin_url( add_query_arg( array( 'page' => 'visual-sitemap' ), 'admin.php' ) ) ); ?>"><?php echo $page->labels->name; ?></a></li>
-						<?php if ( $taxonomy == 'category' ): ?>
-							<li><a class="button" href="<?php echo esc_url( admin_url( add_query_arg( array( 'page' => 'visual-sitemap', 'taxonomy' => 'post_tag' ), 'admin.php' ) ) ); ?>"><?php echo $tag->labels->name; ?></a></li>
-						<?php else: ?>
-							<li><a class="button" href="<?php echo esc_url( admin_url( add_query_arg( array( 'page' => 'visual-sitemap', 'taxonomy' => 'category' ), 'admin.php' ) ) ); ?>"><?php echo $category->labels->name; ?></a></li>
-						<?php endif; ?>
-					</ul>
-
-					<ul class="vs-primary col<?php echo max( sizeof( $terms ), 1 ) + 1; ?>">
-						<li class="vs-home"><a class="button disabled"><?php bloginfo( 'name' ); ?><span class="dashicons dashicons-admin-home"></span></a></li>
-						<?php if ( sizeof( $terms ) ): ?>
-							<?php foreach ( $terms as $term ): ?>
-								<li><a class="button button-primary action" href="<?php echo esc_url( admin_url( add_query_arg( array( 'action' => 'edit', 'taxonomy' => $taxonomy, 'tag_ID' => $term->term_id, 'post_type' => 'post' ), 'edit-tags.php' ) ) ); ?>"><?php echo wp_strip_all_tags( $term->name ); ?><span class="dashicons dashicons-<?php echo $taxonomy == 'category' ? 'category' : 'tag'; ?>"></span></a>
-									<?php
-									if ( $taxonomy == 'category' )
-										$posts = get_posts( array( 'category' => $term->term_id ) );
-									else
-										$posts = get_posts( array( 'tag' => $term->slug ) );
-									?>
-									<?php if ( $posts ): ?>
-										<ul>
-											<?php foreach ( $posts as $post ): ?>
-												<li><a class="button action" href="<?php echo esc_url( get_edit_post_link( $post->ID ) ); ?>"><?php echo wp_strip_all_tags( $post->post_title ); ?><span class="dashicons dashicons-admin-page wp-ui-text-notification"></span></a></li>
-											<?php endforeach; ?>
-										</ul>
-									<?php endif; ?>
-									<?php if ( $taxonomy == 'category' && array_key_exists( $term->term_id, $sub_terms ) ): $subcategories = $sub_terms[ $term->term_id ]; ?>
-										<?php if ( $subcategories ): ?>
-											<ul>
-												<?php foreach ( $subcategories as $subcategory ): ?>
-													<li><a class="button action" href="<?php echo esc_url( admin_url( add_query_arg( array( 'action' => 'edit', 'taxonomy' => $taxonomy, 'tag_ID' => $subcategory->term_id, 'post_type' => 'post' ), 'edit-tags.php' ) ) ); ?>"><?php echo wp_strip_all_tags( $subcategory->name ); ?><span class="dashicons dashicons-<?php echo $taxonomy == 'category' ? 'category' : 'tag'; ?> wp-ui-text-highlight"></span></a>
-														<?php $posts = get_posts( array( $taxonomy => $subcategory->term_id ) ); ?>
-														<?php if ( $posts ): ?>
-															<ul>
-																<?php foreach ( $posts as $post ): ?>
-																	<li><a class="button action" href="<?php echo esc_url( get_edit_post_link( $post->ID ) ); ?>"><?php echo wp_strip_all_tags( $post->post_title ); ?><span class="dashicons dashicons-admin-page wp-ui-text-notification"></span></a></li>
-																<?php endforeach; ?>
-															</ul>
-														<?php endif; ?>
-													</li>
-												<?php endforeach; ?>
-											</ul>
-										<?php endif; ?>
-									<?php endif; ?>
-								</li>
-							<?php endforeach; ?>
-						<?php else: ?>
-							<li><a class="button disabled"><?php echo $object->labels->not_found; ?></a></li>
-						<?php endif; ?>
-						<li><a class="button button-primary action" href="<?php echo esc_url( admin_url( add_query_arg( array( 'taxonomy' => $taxonomy ), 'edit-tags.php' ) ) ); ?>"><?php echo $object->labels->add_new_item; ?><span class="dashicons dashicons-plus"></span></a></li>
-					</ul>
-
-				<?php endif; ?>
-			</div>
 		</div>
 		<p>
 			<a href="http://wordpress.org/support/view/plugin-reviews/visual-sitemap?rate=5#postform">
@@ -239,9 +255,9 @@ class Visual_Sitemap {
 	 * Enqueue styles
 	 */
 	public function admin_styles() {
-		$screen = get_current_screen();
+		//$screen = get_current_screen();
 
-		if ( $screen->id == 'toplevel_page_visual-sitemap' )
+		//if ( $screen->id == 'toplevel_page_visual-sitemap' )
 			wp_enqueue_style( 'visual-sitemap-styles', VISUAL_SITEMAP_URL . '/assets/css/visual-sitemap.css', array(), VISUAL_SITEMAP_VERSION );
 	}
 }
